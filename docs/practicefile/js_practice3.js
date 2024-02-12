77a9b1c9b08d1cb84cb8be07650331500110fd91
Yui9611 <149279458+Yui9611@users.noreply.github.com>
2024-02-12 15:30:36 +0000

    re

--- docs/practicefile/js_practice3.js
+++ docs/practicefile/js_practice3.js
@@ -1,25 +1,29 @@
-function purplebutton{
-    var inputDay = parseInt(document.getElementById('day').value);
-    var inputMonth = parseInt(document.getElementById('month').value) - 1;
-    var inputYear = parseInt(document.getElementById('year').value);
-    
-    var today = new Date(2024, 2, 13);
-    var birthDate = new Date(inputYear, inputMonth, inputDay);
+function purplebutton(){
+    if (customerName.trim() !==""){
+        var inputDay = parseInt(document.getElementById('day').value);
+        var inputMonth = parseInt(document.getElementById('month').value) - 1;
+        var inputYear = parseInt(document.getElementById('year').value);
+        
+        var today = new Date();
+        var birthDate = new Date(inputYear, inputMonth, inputDay);
 
-    var years = today.getFullYear() - birthDate.getFullYear();
-    var months = today.getMonth() - birthDate.getMonth();
-    var days = today.getDate() - birthDate.getDate();
-    if (days < 0) {
-        months--;
-        days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+        var years = today.getFullYear() - birthDate.getFullYear();
+        var months = today.getMonth() - birthDate.getMonth();
+        var days = today.getDate() - birthDate.getDate();
+        if (days < 0) {
+            months--;
+            days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+        }
+
+        if (months < 0) {
+            years--;
+            months += 12;
+        }
+
+        document.getElementById('yearsdisplay').innerText = years;
+        document.getElementById('monthsdisplay').innerText = months;
+        document.getElementById('daysdisplay').innerText = days;
+    }else{
+        
     }
-
-    if (months < 0) {
-        years--;
-        months += 12;
-    }
-
-    document.getElementById('yearsdisplay').innerText = years;
-    document.getElementById('monthsdisplay').innerText = months;
-    document.getElementById('daysdisplay').innerText = days;   
 }
